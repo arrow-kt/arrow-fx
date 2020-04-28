2b6dbe933544e9bce6f3bd1c3346c57e1a70cf21
nomisRev <vergauwen.simon@gmail.com>
2020-04-28 16:55:11 +0200

    Make helper function internal

--- arrow-fx-reactor/src/main/kotlin/arrow/fx/reactor/extensions/common.kt
+++ arrow-fx-reactor/src/main/kotlin/arrow/fx/reactor/extensions/common.kt
@@ -3,5 +3,5 @@ package arrow.fx.reactor.extensions
 import arrow.core.Tuple2
 import java.util.function.BiFunction
 
-fun <A, B> tupled(): BiFunction<A, B, Tuple2<A, B>> =
+internal fun <A, B> tupled(): BiFunction<A, B, Tuple2<A, B>> =
   BiFunction<A, B, Tuple2<A, B>> { t, u -> Tuple2(t, u) }
