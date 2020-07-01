c7a04c6e397d4f811c886d656940e8fa63e65534
Alberto Ballano <aballano@users.noreply.github.com>
2020-07-01 14:57:17 +0200

    Nits

--- arrow-fx-reactor/src/test/kotlin/arrow/fx/FluxKTest.kt
+++ arrow-fx-reactor/src/test/kotlin/arrow/fx/FluxKTest.kt
@@ -29,7 +29,6 @@ import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
 import reactor.core.publisher.Flux
 import reactor.core.scheduler.Schedulers
-import reactor.test.expectError
 import reactor.test.test
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
@@ -136,7 +135,7 @@ class FluxKTest : UnitSpec() {
 
       FluxK.just(Unit)
         .bracketCase(
-          use = { FluxK.async<Nothing> { _, _ -> } },
+          use = { FluxK.async<Nothing> { } },
           release = { _, exitCase ->
             FluxK {
               ec = exitCase
@@ -152,24 +151,11 @@ class FluxKTest : UnitSpec() {
       ec shouldBe ExitCase.Cancelled
     }
 
-    "FluxK should cancel KindConnection on dispose" {
-      Promise.uncancellable<ForFluxK, Unit>(FluxK.async()).flatMap { latch ->
-          FluxK {
-            FluxK.async<Unit> { conn, _ ->
-              conn.push(latch.complete(Unit))
-            }.flux.subscribe().dispose()
-          }.flatMap { latch.get() }
-        }.value()
-        .test()
-        .expectNext(Unit)
-        .expectComplete()
-    }
-
     "FluxK async should be cancellable" {
       Promise.uncancellable<ForFluxK, Unit>(FluxK.async())
         .flatMap { latch ->
           FluxK {
-            FluxK.async<Unit> { _, _ -> }
+            FluxK.async<Unit> { }
               .value()
               .doOnCancel { latch.complete(Unit).value().subscribe() }
               .subscribe()
@@ -179,14 +165,6 @@ class FluxKTest : UnitSpec() {
         .test()
         .expectNext(Unit)
         .expectComplete()
-    }
-
-    "KindConnection can cancel upstream" {
-      FluxK.async<Unit> { connection, _ ->
-          connection.cancel().value().subscribe()
-        }.value()
-        .test()
-        .expectError(ConnectionCancellationException::class)
     }
   }
 }
--- arrow-fx-reactor/src/test/kotlin/arrow/fx/MonoKTest.kt
+++ arrow-fx-reactor/src/test/kotlin/arrow/fx/MonoKTest.kt
@@ -30,7 +30,6 @@ import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
-import reactor.test.expectError
 import reactor.test.test
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
@@ -140,7 +139,7 @@ class MonoKTest : UnitSpec() {
 
       MonoK.just(Unit)
         .bracketCase(
-          use = { MonoK.async<Nothing> { _, _ -> } },
+          use = { MonoK.async<Nothing> { } },
           release = { _, exitCase ->
             MonoK {
               ec = exitCase
@@ -156,24 +155,11 @@ class MonoKTest : UnitSpec() {
       ec shouldBe ExitCase.Cancelled
     }
 
-    "MonoK should cancel KindConnection on dispose" {
-      Promise.uncancellable<ForMonoK, Unit>(MonoK.async()).flatMap { latch ->
-        MonoK {
-          MonoK.async<Unit> { conn, _ ->
-            conn.push(latch.complete(Unit))
-          }.mono.subscribe().dispose()
-        }.flatMap { latch.get() }
-      }.value()
-        .test()
-        .expectNext(Unit)
-        .expectComplete()
-    }
-
     "MonoK async should be cancellable" {
       Promise.uncancellable<ForMonoK, Unit>(MonoK.async())
         .flatMap { latch ->
           MonoK {
-            MonoK.async<Unit> { _, _ -> }
+            MonoK.async<Unit> { }
               .value()
               .doOnCancel { latch.complete(Unit).value().subscribe() }
               .subscribe()
@@ -183,14 +169,6 @@ class MonoKTest : UnitSpec() {
         .test()
         .expectNext(Unit)
         .expectComplete()
-    }
-
-    "KindConnection can cancel upstream" {
-      MonoK.async<Unit> { connection, _ ->
-        connection.cancel().value().subscribe()
-      }.value()
-        .test()
-        .expectError(ConnectionCancellationException::class)
     }
 
     "MonoK should suspend" {
