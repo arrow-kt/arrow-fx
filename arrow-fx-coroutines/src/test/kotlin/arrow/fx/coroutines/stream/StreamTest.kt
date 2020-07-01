2d528d2f7fffe214bd0f436802e02e372525d520
nomisRev <vergauwen.simon@gmail.com>
2020-07-01 16:21:36 +0200

    Less sleep between elements, more time for elements to pass through

--- arrow-fx-coroutines/src/test/kotlin/arrow/fx/coroutines/stream/StreamTest.kt
+++ arrow-fx-coroutines/src/test/kotlin/arrow/fx/coroutines/stream/StreamTest.kt
@@ -776,19 +776,21 @@ class StreamTest : StreamSpec(spec = {
           loop(0, it.asPull()).stream()
         }
 
-        val start = System.currentTimeMillis()
+        // val start = System.currentTimeMillis()
 
         Stream.iterate(0, Int::inc)
-          .flatMap { Stream(it).delayBy(10.milliseconds) }
-          .interruptWhen { Right(sleep(150.milliseconds)) }
+          .flatMap { Stream(it).delayBy(5.milliseconds) }
+          .interruptWhen { Right(sleep(200.milliseconds)) }
           .through(p)
           .compile()
-          .foldChunks(mutableListOf<Int>()) { acc, ch ->
-            println("First: $ch after ${System.currentTimeMillis() - start}")
-            acc.addAll(ch.toList())
-            acc
-          }.let { result ->
-            println("Finished: $result after ${System.currentTimeMillis() - start}")
+          // .foldChunks(mutableListOf<Int>()) { acc, ch ->
+          //   println("First: $ch after ${System.currentTimeMillis() - start}")
+          //   acc.addAll(ch.toList())
+          //   acc
+          // }
+          .toList()
+        .let { result ->
+            // println("Finished: $result after ${System.currentTimeMillis() - start}")
             result shouldBe listOfNotNull(result.firstOrNull()) + result.drop(1).filter { it != 0 }
           }
       }
