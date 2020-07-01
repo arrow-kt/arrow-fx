45dfd621fbad0137c6ea831d93e69cc3c7bbf317
nomisRev <vergauwen.simon@gmail.com>
2020-07-01 10:02:35 +0200

    Add concurrently

--- arrow-fx-coroutines/src/test/kotlin/arrow/fx/coroutines/stream/Arb.kt
+++ arrow-fx-coroutines/src/test/kotlin/arrow/fx/coroutines/stream/Arb.kt
@@ -9,6 +9,7 @@ import io.kotest.property.arbitrary.arb
 import io.kotest.property.arbitrary.bool
 import io.kotest.property.arbitrary.byte
 import io.kotest.property.arbitrary.choice
+import io.kotest.property.arbitrary.choose
 import io.kotest.property.arbitrary.create
 import io.kotest.property.arbitrary.double
 import io.kotest.property.arbitrary.float
@@ -43,16 +44,21 @@ inline fun <reified A> Arb.Companion.array(
   }
 }
 
-fun <A, B> arrayChunkGenerator(
-  gen: Gen<A>,
+@PublishedApi
+internal fun <A, B> arrayChunkGenerator(
+  arb: Arb<A>,
+  shrinker: Shrinker<B>,
   range: IntRange = 0..10,
   build: (values: List<A>, offset: Int, length: Int) -> B
 ): Arb<B> {
   check(!range.isEmpty())
   check(range.first >= 0)
 
-  return arb {
-    val genIter = gen.generate(it).iterator()
+  val edgecases =
+    arb.edgecases().map { a -> build(listOf(a), 0, 1) } + build(emptyList(), 0, 0)
+
+  return arb(edgecases, shrinker) {
+    val genIter = arb.generate(it).iterator()
 
     sequence {
       while (true) {
@@ -86,7 +92,7 @@ class ChunkShrinker<A> : Shrinker<Chunk<A>> {
 }
 
 inline fun <reified A> Arb.Companion.chunk(arb: Arb<A>): Arb<Chunk<A>> =
-  Arb.frequency(
+  Arb.choose(
     1 to Arb.create { Chunk.empty<A>() },
     5 to arb.map { Chunk.just(it) },
     10 to Arb.list(arb, 0..20).map { Chunk.iterable(it) },
@@ -96,99 +102,76 @@ inline fun <reified A> Arb.Companion.chunk(arb: Arb<A>): Arb<Chunk<A>> =
   )
 
 inline fun <reified A> Arb.Companion.boxedChunk(arb: Arb<A>): Arb<Chunk<A>> =
-  arrayChunkGenerator(arb) { values, offset, length ->
+  arrayChunkGenerator(arb, ChunkShrinker()) { values, offset, length ->
     Chunk.boxed(values.toTypedArray(), offset, length)
   }
 
 fun Arb.Companion.booleanChunk(): Arb<Chunk<Boolean>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.bool()) { values, offset, length ->
+    arrayChunkGenerator(Arb.bool(), ChunkShrinker()) { values, offset, length ->
       Chunk.booleans(values.toBooleanArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.bool()) { values, _, _ ->
+    arrayChunkGenerator(Arb.bool(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
 fun Arb.Companion.byteChunk(): Arb<Chunk<Byte>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.byte()) { values, offset, length ->
+    arrayChunkGenerator(Arb.byte(), ChunkShrinker()) { values, offset, length ->
       Chunk.bytes(values.toByteArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.byte()) { values, _, _ ->
+    arrayChunkGenerator(Arb.byte(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
-fun Arb.Companion.intChunk(range: IntRange = 0..100): Arb<Chunk<Int>> =
+fun Arb.Companion.intChunk(): Arb<Chunk<Int>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.int(), range) { values, offset, length ->
+    arrayChunkGenerator(Arb.int(), ChunkShrinker()) { values, offset, length ->
       Chunk.ints(values.toIntArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.int(), range) { values, _, _ ->
+    arrayChunkGenerator(Arb.int(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
 fun Arb.Companion.longChunk(): Arb<Chunk<Long>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.long()) { values, offset, length ->
+    arrayChunkGenerator(Arb.long(), ChunkShrinker()) { values, offset, length ->
       Chunk.longs(values.toLongArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.long()) { values, _, _ ->
+    arrayChunkGenerator(Arb.long(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
 fun Arb.Companion.doubleChunk(): Arb<Chunk<Double>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.double()) { values, offset, length ->
+    arrayChunkGenerator(Arb.double(), ChunkShrinker()) { values, offset, length ->
       Chunk.doubles(values.toDoubleArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.double()) { values, _, _ ->
+    arrayChunkGenerator(Arb.double(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
 fun Arb.Companion.floatChunk(): Arb<Chunk<Float>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.float()) { values, offset, length ->
+    arrayChunkGenerator(Arb.float(), ChunkShrinker()) { values, offset, length ->
       Chunk.floats(values.toFloatArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.float()) { values, _, _ ->
+    arrayChunkGenerator(Arb.float(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray())
     }
   )
 
 fun Arb.Companion.shortChunk(): Arb<Chunk<Short>> =
   Arb.choice(
-    arrayChunkGenerator(Arb.short()) { values, offset, length ->
+    arrayChunkGenerator(Arb.short(), ChunkShrinker()) { values, offset, length ->
       Chunk.shorts(values.toShortArray(), offset, length)
     },
-    arrayChunkGenerator(Arb.short()) { values, _, _ ->
+    arrayChunkGenerator(Arb.short(), ChunkShrinker()) { values, _, _ ->
       Chunk.array(values.toTypedArray<Short>())
     }
   )
-
-fun <A> Arb.Companion.frequency(vararg arbs: Pair<Int, Arb<A>>): Arb<A> =
-  arb(arbs.flatMap { it.second.edgecases() }) { rs ->
-    require(arbs.isNotEmpty()) { "No Arb instances passed to Arb.frequency()." }
-
-    val iters = arbs.toList().flatMap { (weight, arb) ->
-      List(weight) { arb }
-    }.map { it.values(rs).iterator() }
-
-    fun next(): Sample<A>? {
-      val iter = iters.shuffled(rs.random).first()
-      return if (iter.hasNext()) iter.next() else null
-    }
-
-    sequence {
-      while (true) {
-        var next: Sample<A>? = null
-        while (next == null)
-          next = next()
-        yield(next.value)
-      }
-    }
-  }
