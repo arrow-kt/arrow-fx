e6a13db24c91de4796a1282c3e0d6ce97ba807c9
Alberto Ballano <aballano@users.noreply.github.com>
2020-07-30 16:57:57 +0200

    Temporarily disable limited iterations

--- arrow-fx/src/test/kotlin/arrow/fx/ArrowFxSpec.kt
+++ arrow-fx/src/test/kotlin/arrow/fx/ArrowFxSpec.kt
@@ -1,9 +1,9 @@
 package arrow.fx
 
 import arrow.core.test.UnitSpec
-import io.kotest.property.Arb
-import io.kotest.property.PropertyContext
-import io.kotest.property.forAll
+//import io.kotest.property.Arb
+//import io.kotest.property.PropertyContext
+//import io.kotest.property.forAll
 
 abstract class ArrowFxSpec(
   private val iterations: Int = 350,
@@ -14,93 +14,93 @@ abstract class ArrowFxSpec(
     spec()
   }
 
-  suspend fun <A> forAll(
-    genA: Arb<A>,
-    property: PropertyContext.(A) -> Boolean
-  ) =
-    forAll(
-      iterations,
-      genA,
-      property
-    )
-
-  suspend fun <A, B> forAll(
-    genA: Arb<A>,
-    genB: Arb<B>,
-    property: PropertyContext.(A, B) -> Boolean
-  ) =
-    forAll(
-      iterations,
-      genA,
-      genB,
-      property
-    )
-
-  suspend fun <A, B, C> forAll(
-    genA: Arb<A>,
-    genB: Arb<B>,
-    genC: Arb<C>,
-    property: PropertyContext.(A, B, C) -> Boolean
-  ): Unit =
-    forAll(
-      iterations,
-      genA,
-      genB,
-      genC,
-      property
-    )
-
-  suspend fun <A, B, C, D> forAll(
-    genA: Arb<A>,
-    genB: Arb<B>,
-    genC: Arb<C>,
-    genD: Arb<D>,
-    property: PropertyContext.(A, B, C, D) -> Boolean
-  ): Unit =
-    forAll(
-      iterations,
-      genA,
-      genB,
-      genC,
-      genD,
-      property
-    )
-
-  suspend fun <A, B, C, D, E> forAll(
-    genA: Arb<A>,
-    genB: Arb<B>,
-    genC: Arb<C>,
-    genD: Arb<D>,
-    genE: Arb<E>,
-    property: PropertyContext.(A, B, C, D, E) -> Boolean
-  ): Unit =
-    forAll(
-      iterations,
-      genA,
-      genB,
-      genC,
-      genD,
-      genE,
-      property
-    )
-
-  suspend fun <A, B, C, D, E, F> forAll(
-    genA: Arb<A>,
-    genB: Arb<B>,
-    genC: Arb<C>,
-    genD: Arb<D>,
-    genE: Arb<E>,
-    genF: Arb<F>,
-    property: PropertyContext.(A, B, C, D, E, F) -> Boolean
-  ): Unit =
-    forAll(
-      iterations,
-      genA,
-      genB,
-      genC,
-      genD,
-      genE,
-      genF,
-      property
-    )
+//  suspend fun <A> forAll(
+//    genA: Arb<A>,
+//    property: PropertyContext.(A) -> Boolean
+//  ) =
+//    forAll(
+//      iterations,
+//      genA,
+//      property
+//    )
+//
+//  suspend fun <A, B> forAll(
+//    genA: Arb<A>,
+//    genB: Arb<B>,
+//    property: PropertyContext.(A, B) -> Boolean
+//  ) =
+//    forAll(
+//      iterations,
+//      genA,
+//      genB,
+//      property
+//    )
+//
+//  suspend fun <A, B, C> forAll(
+//    genA: Arb<A>,
+//    genB: Arb<B>,
+//    genC: Arb<C>,
+//    property: PropertyContext.(A, B, C) -> Boolean
+//  ): Unit =
+//    forAll(
+//      iterations,
+//      genA,
+//      genB,
+//      genC,
+//      property
+//    )
+//
+//  suspend fun <A, B, C, D> forAll(
+//    genA: Arb<A>,
+//    genB: Arb<B>,
+//    genC: Arb<C>,
+//    genD: Arb<D>,
+//    property: PropertyContext.(A, B, C, D) -> Boolean
+//  ): Unit =
+//    forAll(
+//      iterations,
+//      genA,
+//      genB,
+//      genC,
+//      genD,
+//      property
+//    )
+//
+//  suspend fun <A, B, C, D, E> forAll(
+//    genA: Arb<A>,
+//    genB: Arb<B>,
+//    genC: Arb<C>,
+//    genD: Arb<D>,
+//    genE: Arb<E>,
+//    property: PropertyContext.(A, B, C, D, E) -> Boolean
+//  ): Unit =
+//    forAll(
+//      iterations,
+//      genA,
+//      genB,
+//      genC,
+//      genD,
+//      genE,
+//      property
+//    )
+//
+//  suspend fun <A, B, C, D, E, F> forAll(
+//    genA: Arb<A>,
+//    genB: Arb<B>,
+//    genC: Arb<C>,
+//    genD: Arb<D>,
+//    genE: Arb<E>,
+//    genF: Arb<F>,
+//    property: PropertyContext.(A, B, C, D, E, F) -> Boolean
+//  ): Unit =
+//    forAll(
+//      iterations,
+//      genA,
+//      genB,
+//      genC,
+//      genD,
+//      genE,
+//      genF,
+//      property
+//    )
 }
