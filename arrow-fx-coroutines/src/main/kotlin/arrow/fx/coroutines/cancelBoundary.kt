1aff17a048146c5ba0564584b0c81de1b3ca5c6b
Simon Vergauwen <vergauwen.simon@gmail.com>
2020-08-10 16:48:50 +0200

    Add example cancelBoundary

--- arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/cancelBoundary.kt
+++ arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/cancelBoundary.kt
@@ -6,6 +6,28 @@ import kotlin.coroutines.intrinsics.suspendCoroutineUninterceptedOrReturn
 /**
  * Checks for cancellation,
  * when cancellation occurs this coroutine will suspend indefinitely and never continue.
+ *
+ * ```kotlin:ank:playground
+ * import arrow.fx.coroutines.*
+ *
+ * //sampleStart
+ * suspend fun forever(): Unit {
+ *   while(true) {
+ *     println("I am getting dizzy...")
+ *     cancelBoundary() // cancellable computation loop
+ *   }
+ * }
+ *
+ * suspend fun main(): Unit {
+ *   val fiber = ForkConnected {
+ *     guaranteeCase({ forever() }) { exitCase ->
+ *       println("forever finished with $exitCase")
+ *     }
+ *   }
+ *   sleep(10.milliseconds)
+ *   fiber.cancel()
+ * }
+ * ```
  */
 suspend fun cancelBoundary(): Unit =
   suspendCoroutineUninterceptedOrReturn { cont ->
