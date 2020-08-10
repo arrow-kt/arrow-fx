f3095f5f23e9482d4e8ce9ccf8489d5baa12ad4e
Simon Vergauwen <vergauwen.simon@gmail.com>
2020-08-10 16:49:20 +0200

    Add docs dispatchers

--- arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/dispatchers.kt
+++ arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/dispatchers.kt
@@ -8,23 +8,43 @@ import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
-val ComputationPool: CoroutineContext = ForkJoinPool().asCoroutineContext()
+
+/**
+ * A [CoroutineContext] to run non-blocking suspending code,
+ * all code that relies on blocking IO should prefer to use an unbounded [IOPool].
+ *
+ * A work-stealing thread pool using all available processors as its target parallelism level.
+ */
+val ComputationPool: CoroutineContext =
+  ForkJoinPool().asCoroutineContext()
 
 private object IOCounter {
   private val ref = atomic(0)
   fun getAndIncrement(): Int = ref.getAndIncrement()
 }
 
-val IOPool = Executors.newCachedThreadPool { r ->
-  Thread(r).apply {
-    name = "io-arrow-kt-worker-${IOCounter.getAndIncrement()}"
-    isDaemon = true
-  }
-}.asCoroutineContext()
+/**
+ * Creates a thread pool that creates new threads as needed, but
+ * will reuse previously constructed threads when they are available, and uses the provided.
+ *
+ * This pool is prone to cause [OutOfMemoryError] since the pool size is unbounded.
+ */
+val IOPool: CoroutineContext =
+  Executors.newCachedThreadPool { r ->
+    Thread(r).apply {
+      name = "io-arrow-kt-worker-${IOCounter.getAndIncrement()}"
+      isDaemon = true
+    }
+  }.asCoroutineContext()
+
 
 internal fun ExecutorService.asCoroutineContext(): CoroutineContext =
   ExecutorServiceContext(this)
 
+/**
+ * Wraps an [ExecutorService] in a [CoroutineContext] as a [ContinuationInterceptor]
+ * scheduling on the [ExecutorService] when [kotlin.coroutines.intrinsics.intercepted] is called.
+ */
 private class ExecutorServiceContext(val pool: ExecutorService) :
   AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
   override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> =
@@ -34,6 +54,7 @@ private class ExecutorServiceContext(val pool: ExecutorService) :
     })
 }
 
+/** Wrap existing continuation to resumes itself on the provided [ExecutorService] */
 private class ExecutorServiceContinuation<T>(val pool: ExecutorService, val cont: Continuation<T>) : Continuation<T> {
   override val context: CoroutineContext = cont.context
 
