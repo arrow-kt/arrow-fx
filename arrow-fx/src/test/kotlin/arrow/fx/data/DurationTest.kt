43a61c457f235ea42b719177b5227a46a5c7d173
Aleksandr Bochev <aleksandr.bochev@zalando.de>
2020-08-21 15:03:17 +0200

    fixed duration generator import

--- arrow-fx/src/test/kotlin/arrow/fx/data/DurationTest.kt
+++ arrow-fx/src/test/kotlin/arrow/fx/data/DurationTest.kt
@@ -1,20 +1,20 @@
 package arrow.fx.data
 
 import arrow.core.test.UnitSpec
-import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
+import arrow.fx.test.generators.duration
 
 class DurationTest : UnitSpec() {
 
   init {
     "plus should be commutative" {
-      forAll(Gen.duration(), Gen.duration()) { a, b ->
+      forAll(duration(), duration()) { a, b ->
         a + b == b + a
       }
     }
 
     "comparison should correct in both directions" {
-      forAll(Gen.duration(), Gen.duration()) { a, b ->
+      forAll(duration(), duration()) { a, b ->
         a < b == b > a
       }
     }
