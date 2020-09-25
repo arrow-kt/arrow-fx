8b71fb2afe74755028cda4a865e0f794d5f115b7
Pablisco <pabs87@gmail.com>
2020-09-25 23:42:01 +0100

    Result doesn't need to be nullable

--- arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/stream/TerminalOps.kt
+++ arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/stream/TerminalOps.kt
@@ -59,7 +59,7 @@ suspend fun <O> Stream<O>.firstOrNull(): O? =
  * This a terminal operator, meaning this functions `suspend`s until the [Stream] finishes.
  * If any errors are raised while streaming, it's thrown from this `suspend` scope.
  */
-suspend fun <O> Stream<O>.firstOrError(): O? =
+suspend fun <O> Stream<O>.firstOrError(): O =
   firstOrNull() ?: throw NoSuchElementException()
 
 /**
