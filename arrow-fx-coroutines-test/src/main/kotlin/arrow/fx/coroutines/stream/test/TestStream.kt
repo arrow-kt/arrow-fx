08d3b7ddb7ec1b0af4b5fe84e5b0449d524e8152
Pablisco <pabs87@gmail.com>
2020-09-30 01:10:32 +0100

    Add extra features and tests

--- arrow-fx-coroutines-test/src/main/kotlin/arrow/fx/coroutines/stream/test/TestStream.kt
+++ arrow-fx-coroutines-test/src/main/kotlin/arrow/fx/coroutines/stream/test/TestStream.kt
@@ -1,10 +1,14 @@
 package arrow.fx.coroutines.stream.test
 
+import arrow.core.getOrElse
+import arrow.core.orElse
 import arrow.fx.coroutines.stream.Stream
 import arrow.fx.coroutines.stream.concurrent.Queue
 import arrow.fx.coroutines.stream.drain
 import arrow.fx.coroutines.stream.firstOrNull
 import arrow.fx.coroutines.stream.toList
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
@@ -27,10 +31,23 @@ fun testStreamCompat(
 // TODO: Convert to Kotlin Duration when not experimental
 class TestStream internal constructor(private val timeout: ArrowDuration) {
 
-  suspend fun Stream<*>.capture(): Unit = through(queue.enqueue()).drain()
+  fun Stream<*>.capture() {
+    GlobalScope.launch {
+      runCatching { through(queue.enqueue()).drain() }
+        .onFailure { exceptionQueue.enqueue1(it) }
+    }
+  }
 
   suspend inline fun expect(item: Any?): Unit = next().let { next ->
     check(next == item) { "Expected $item but got $next" }
+  }
+
+  suspend inline fun expect(vararg items: Any?): Unit = next(items.size).let { next ->
+    check(next.contentEquals(items)) { "Expected $items but got $next" }
+  }
+
+  suspend inline fun expectException(exception: Throwable): Unit = nextException().let { next ->
+    check(next == exception) { "Expected $exception but got $next" }
   }
 
   suspend fun expectNothingMore() {
@@ -38,12 +55,21 @@ class TestStream internal constructor(private val timeout: ArrowDuration) {
     check(values.isEmpty()) { "Expected nothing more but got: $values" }
   }
 
-  private val queue = Queue.unsafeUnbounded<Any?>()
+  suspend fun next(): Any? = queue.next()
 
-  suspend fun next(): Any? =
-    queue.dequeue().interruptAfter(timeout).firstOrError { "Timeout after $timeout" }
+  suspend fun nextException(): Any? = exceptionQueue.next()
+
+  suspend fun next(n: Int) =
+    queue.dequeue().timeout(timeout).take(n).toList().toTypedArray()
+
+  private val queue = Queue.unsafeUnbounded<Any?>()
+  private val exceptionQueue = Queue.unsafeUnbounded<Throwable>()
+
+  private suspend fun <O> Queue<O>.next(): O =
+    dequeue().interruptAfter(timeout).firstOrError { "Timeout after $timeout" }
 
 }
+
 
 private suspend fun <O> Stream<O>.firstOrError(message: () -> String) =
   firstOrNull() ?: error(message())
--- arrow-fx-coroutines-test/src/test/kotlin/arrow/fx/coroutines/stram/test/TestStreamTest.kt
+++ arrow-fx-coroutines-test/src/test/kotlin/arrow/fx/coroutines/stram/test/TestStreamTest.kt
@@ -1,10 +1,19 @@
 package arrow.fx.coroutines.stram.test
 
 import arrow.fx.coroutines.stream.Stream
+import arrow.fx.coroutines.stream.concurrent.Queue
 import arrow.fx.coroutines.stream.test.testStreamCompat
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class TestStreamTest : StringSpec({
+
+  "expects nothing when stream is empty" {
+    testStreamCompat {
+      Stream.empty<String>().capture()
+      expectNothingMore()
+    }
+  }
 
   "expects single item" {
     testStreamCompat {
@@ -13,10 +22,44 @@ class TestStreamTest : StringSpec({
     }
   }
 
-//  @Test
-//  fun `expects single item`(): Unit = testStreamCompat {
-//    Stream.just("Hello").capture()
-//    expect("Hello")
-//  }
+  "expects multiple items" {
+    testStreamCompat {
+      Stream("Hello", "World").capture()
+
+      expect("Hello", "World")
+    }
+  }
+
+  "expects items in order" {
+    testStreamCompat {
+      val queue = Queue.unbounded<String>()
+      queue.dequeue().capture()
+
+      queue.enqueue1("Hello")
+      expect("Hello")
+      queue.enqueue1("World")
+      expect("World")
+    }
+  }
+
+  "expects exception" {
+    testStreamCompat {
+      val expectedException = RuntimeException()
+
+      Stream.raiseError<RuntimeException>(expectedException).capture()
+
+      expectException(expectedException)
+    }
+  }
+
+  // TODO: Not working ATM
+  "handle multiple streams".config(enabled = false) {
+    testStreamCompat {
+      Stream("Hello").capture()
+      Stream("World").capture()
+
+      expect("Hello", "World")
+    }
+  }
 
 })
